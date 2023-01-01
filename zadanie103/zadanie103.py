5316710331447d2bbd2395b8323bd5d7bae9d10f
Kamil <kamil2001p@gmail.com>
2023-01-01 20:16:48 +0100

    103 - 104(nearly finished)

--- zadanie103/zadanie103.py
+++ zadanie103/zadanie103.py
@@ -2,7 +2,6 @@ import re
 
 url = input("Podaj adres url: ")
 pattern = re.compile("^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$")
-
 if pattern.match(url):
     print("Ten adres jest poprawny")
 else:
